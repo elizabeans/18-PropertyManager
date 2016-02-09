c7068935c6801b65080372dec8429dc2fe8ee13d
Elizabeth Maraan <Elizabeth Maraan>
2016-02-09 09:54:05 -0800

    initial commit, completed controllers

---
+++ PropertyManager/PropertyManager/Domain/Property.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PropertyManager.Models;
+
+namespace PropertyManager.Domain
+{
+    public class Property
+    {
+        public Property()
+        {
+        }
+
+        public Property(PropertyModel model)
+        {
+            this.Update(model);
+        }
+
+        public int PropertyId { get; set; }
+        public int? AddressId { get; set; }
+        public string PropertyName { get; set; } 
+        public int? SquareFeet { get; set; }        // ? after type means nullable, don't have to put after strings because strings are nullable by default
+        public int? NumberOfBedrooms { get; set; }
+        public float? NumberOfBathrooms { get; set; }
+        public int? NumberOfVehicles { get; set; }
+        
+        public virtual Address Address { get; set; }
+
+        public virtual ICollection<Lease> Leases { get; set; }
+        public virtual ICollection<WorkOrder> WorkOrders { get; set; }
+
+        public void Update(PropertyModel model)
+        {
+            AddressId = model.AddressId;
+            PropertyName = model.PropertyName;
+            SquareFeet = model.SquareFeet;
+            NumberOfBedrooms = model.NumberOfBedrooms;
+            NumberOfBathrooms = model.NumberOfBathrooms;
+            NumberOfVehicles = model.NumberOfVehicles;
+        }
+    }
+}
---
+++ PropertyManager/PropertyManager/Domain/WorkOrder.cs
@@ -0,0 +1,48 @@
+﻿using PropertyManager.Models;
+using System;
+
+namespace PropertyManager.Domain
+{
+    public enum Priorities
+    {
+        Critical = 1,
+        Major = 2,
+        High = 3,
+        Medium = 4,
+        Low = 5
+    }
+
+    public class WorkOrder
+    {
+        public WorkOrder()
+        {
+
+        }
+
+        public WorkOrder(WorkOrderModel model)
+        {
+            this.Update(model);
+        }
+
+        public int WorkOrderId { get; set; }
+        public int PropertyId { get; set; }
+        public int? TenantId { get; set; }
+        public string Description { get; set; }
+        public Priorities Priority { get; set; }
+        public DateTime OpenedDate { get; set; }
+        public DateTime ClosedDate { get; set; }
+
+        public virtual Property Property { get; set; }
+        public virtual Tenant Tenant { get; set; }
+
+        public void Update(WorkOrderModel model)
+        {
+            PropertyId = model.PropertyId;
+            TenantId = model.TenantId;
+            Description = model.Description;
+            Priority = model.Priority;
+            OpenedDate = model.OpenedDate;
+            ClosedDate = model.ClosedDate;
+        }
+    }
+}
